a4fba06584930596ff7a8d6a887f2eb4b96b76e3
pirapakaran sajeevan <prabaharansajeevan23@gmail.com>
2025-04-23 18:50:58 +0530

    code

--- login/js/login_js.js
+++ login/js/login_js.js
@@ -4,11 +4,11 @@ function login() {
     if (email === "ngo@example.com" && password === "ngo1234") {
       // If NGO login successful
       localStorage.setItem("role", "ngo");
-      window.location.href = "/NGO/html/home.html"; 
+      window.location.href = "NGO/html/home.html"; 
     } else if (email === "restaurant@example.com" && password === "restaurant1234") {
       // If Restaurant login successful
       localStorage.setItem("role", "restaurant");
-      window.location.href = "/owner/html/index.html"; 
+      window.location.href = "owner/html/index.html"; 
     } else {
       alert("Invalid credentials. Please try again.");
     }
