7c504a14e25b267482783dc53f85ea488bbc9a9d
pirapakaran sajeevan <162146775+prabasajee@users.noreply.github.com>
2025-04-23 19:06:13 +0530

    Update login_js.js

--- login/js/login_js.js
+++ login/js/login_js.js
@@ -1,15 +1,16 @@
 function login() {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    if (email === "ngo@example.com" && password === "ngo1234") {
-      // If NGO login successful
-      localStorage.setItem("role", "ngo");
-      window.location.href = "../../NGO/html/home.html";
-    } else if (email === "restaurant@example.com" && password === "restaurant1234") {
-      // If Restaurant login successful
-      localStorage.setItem("role", "restaurant");
-      window.location.href = "../../owner/html/index.html"; 
-    } else {
-      alert("Invalid credentials. Please try again.");
-    }
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (email === "ngo@example.com" && password === "ngo1234") {
+    // If NGO login successful
+    localStorage.setItem("role", "ngo");
+    window.location.href = "/food-donation-pipeline-project/NGO/html/home.html";
+  } else if (email === "restaurant@example.com" && password === "restaurant1234") {
+    // If Restaurant login successful
+    localStorage.setItem("role", "restaurant");
+    window.location.href = "/food-donation-pipeline-project/login/html/owner/html/index.html";
+  } else {
+    alert("Invalid credentials. Please try again.");
   }
+}
